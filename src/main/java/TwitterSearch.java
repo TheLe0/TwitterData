2404f25c888dc51a3e3260d6f7e0cde336ae0039
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-03 22:38:29 -0300

    Update TwitterSearch.java

--- src/main/java/TwitterSearch.java
+++ src/main/java/TwitterSearch.java
@@ -1,11 +1,38 @@
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class TwitterSearch {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Query query;
+    private QueryResult result;
+    private ConfigurationBuilder cb;
+    private TwitterFactory tf;
+    private Twitter twitter;
+
+    public TwitterSearch() {
+
+        this.cb = new ConfigurationBuilder();
+        this.cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("//TODO")
+                .setOAuthConsumerSecret("//TODO")
+                .setOAuthAccessToken("//TODO")
+                .setOAuthAccessTokenSecret("//TODO");
+        this.tf = new TwitterFactory(cb.build());
+        this.twitter = tf.getInstance();
     }
+
+    public void search() {
+
+        try {
+            this.query = new Query("source:twitter4j yusukey");
+            this.result = this.twitter.search(query);
+
+            for (Status status : result.getTweets()) {
+                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        } catch(TwitterException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
