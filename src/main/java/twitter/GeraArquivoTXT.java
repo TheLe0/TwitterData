e41ee00844042ad808006d49e88bafd9c3c54e5b
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-14 21:37:04 -0300

    Create GeraArquivoTXT.java

---
+++ src/main/java/twitter/GeraArquivoTXT.java
@@ -0,0 +1,40 @@
+package twitter;
+
+import java.io.*;
+import java.net.URL;
+
+public class GeraArquivoTXT {
+    private File file;
+    private FileWriter fw;
+    private BufferedWriter bw;
+
+    public GeraArquivoTXT() {
+        this.open();
+    }
+
+    public void open() {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource("tweets.txt");
+        this.file = new File(resource.getFile());
+
+
+    }
+
+    public void write(String linha) throws IOException {
+        if (this.file == null) return;
+
+        try {
+            this.fw = new FileWriter(this.file, true);
+            this.bw = new BufferedWriter(this.fw);
+
+            this.bw.write(linha);
+            this.bw.newLine();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.bw.close();
+        }
+
+    }
+}
