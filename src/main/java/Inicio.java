62d341204f3df2e5c4c3ebf6963bc405e3f00f2b
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-14 21:34:52 -0300

    Update Inicio.java

--- src/main/java/Inicio.java
+++ src/main/java/Inicio.java
@@ -1,14 +1,12 @@
+import montaArquivos.GeraArquivoHashtags;
+
 import java.io.IOException;
 
 public class Inicio {
-    public static void main(String[] args) {
-        try {
-            TwitterSearch twitter = new TwitterSearch();
-            twitter.search();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
 
-
+        GeraArquivoHashtags obj = new GeraArquivoHashtags();
+        obj.open();
+        obj.monta();
     }
 }
