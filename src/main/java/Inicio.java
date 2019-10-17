f2e45877703f1f78df4131122cf33fe2a64ec8bc
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-17 20:46:18 -0300

    Update Inicio.java

--- src/main/java/Inicio.java
+++ src/main/java/Inicio.java
@@ -1,12 +1,8 @@
-import montaArquivos.GeraArquivoHashtags;
-
 import java.io.IOException;
 
 public class Inicio {
     public static void main(String[] args) throws IOException {
 
-        GeraArquivoHashtags obj = new GeraArquivoHashtags();
-        obj.open();
-        obj.monta();
+
     }
 }
