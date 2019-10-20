0fb2ee02a099b79c28cc8d54d203bd3b405b4125
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-20 20:41:10 -0300

    Update Arquivo.java

--- src/main/java/arquivo/Arquivo.java
+++ src/main/java/arquivo/Arquivo.java
@@ -3,7 +3,8 @@ package arquivo;
 import java.io.*;
 import java.net.URL;
 
-abstract class Arquivo {
+abstract class Arquivo
+{
 
     protected String nomeArquivo;
     protected FileReader reader;
@@ -12,8 +13,14 @@ abstract class Arquivo {
     protected BufferedWriter bw;
     protected File fileIn;
     protected File fileOut;
+    protected String idTwitter;
+    protected String name;
+    protected String tweetContent;
+    protected String sendDate;
+    protected String hashtags;
 
-    public void open() {
+    public void open()
+    {
 
         ClassLoader classLoader = getClass().getClassLoader();
         URL resource = classLoader.getResource(this.nomeArquivo);
@@ -21,7 +28,34 @@ abstract class Arquivo {
 
     }
 
-    public void close() throws IOException {
+    public void close() throws IOException
+    {
         this.bw.close();
+    }
+
+    public void limitFields(String str, DataTypes type)
+    {
+        switch(type)
+        {
+            case ID_TWITTER:
+                str = str.substring(0,18).trim();
+                this.idTwitter = str;
+                break;
+            case NAME:
+                str = str.substring(0,25).trim();
+                this.name = str;
+                break;
+            case CONTENT:
+                str = str.substring(0,255).trim();
+                this.tweetContent = str;
+                break;
+            case DATE:
+                str = str.substring(0,9).trim();
+                this.sendDate = str;
+                break;
+            case HASHTAGS:
+                str = str.substring(0,10);
+                this.hashtags = str;
+        }
     }
 }
