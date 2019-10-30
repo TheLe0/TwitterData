b848ea6a74d96be11a740fc23050e4a9c110e469
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2019-10-30 19:43:43 -0300

    Update Inicio.java

--- src/main/java/Inicio.java
+++ src/main/java/Inicio.java
@@ -1,12 +1,16 @@
-import arquivo.ArquivoTexto;
+import twitter.TwitterSearch;
 
 import java.io.IOException;
 
 public class Inicio {
     public static void main(String[] args) throws IOException {
+        try {
 
-        ArquivoTexto obj = new ArquivoTexto();
-        obj.setArquivoIn("tweets_limited.txt");
-        obj.writeLImited();
+            TwitterSearch twitter = new TwitterSearch();
+            twitter.search();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
